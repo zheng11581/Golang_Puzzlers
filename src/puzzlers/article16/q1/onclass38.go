cecb11617e89755775db36f20caf780356596c73
zheng11581 <zheng1581@sina.com>
2023-09-27 18:02:41 +0800

    article16 demo39 onclass

--- src/puzzlers/article16/q1/onclass38.go
+++ src/puzzlers/article16/q1/onclass38.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	for i := 0; i < 10; i++ {
-		go func() {
+		go func() { // go 函数执行会明显滞后于go 语句
 			fmt.Println(i)
 		}()
 	}
---
+++ src/puzzlers/article16/q2/onclass39.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	// "time"
+)
+
+func main() {
+	num := 10
+	sign := make(chan struct{}, num)
+	for i := 0; i < 10; i++ {
+		go func() { // go函数执行会明显滞后于go语句
+			fmt.Println(i)
+			sign <- struct{}{}
+		}()
+	}
+
+	// 方法1 sleep阻塞主goroutin，等待go函数执行完
+	// time.Sleep(time.Millisecond * 500)
+
+	// 方法2 channel阻塞主goroutin，等待go函数执行完
+	for j := 0; j < 10; j++ {
+		<-sign
+	}
+
+}
