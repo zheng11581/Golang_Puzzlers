3928786e53bca5515af009d85c6c857272f2ea99
zheng11581 <zhhcheng@yonyou.com>
2024-06-18 10:13:14 +0800

    time Format Parse

--- src/foolish/timeformat/main.go
+++ src/foolish/timeformat/main.go
@@ -7,10 +7,14 @@ import (
 )
 
 func main() {
-	format := "02/Jan/2006:15:04:05 -0700"
-	parse, err := time.Parse(format, "17/Jun/2024:06:50:26 +0000")
+	layout := "02/Jan/2006:15:04:05 -0700"
+
+	parse, err := time.Parse(layout, "17/Jun/2024:06:50:26 +0000")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(parse)
+	fmt.Printf("Parse string to time: %v\n", parse)
+
+	format := time.Now().Format(layout)
+	fmt.Printf("Format time to string: %v\n", format)
 }
